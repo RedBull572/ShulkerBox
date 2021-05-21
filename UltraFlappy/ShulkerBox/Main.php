ada1292668a1103ac3dc9671191e33db745c1e94
Flappy <60063027+UltraFlappy@users.noreply.github.com>
2021-05-21 19:58:53 +0700

    Add files via upload

---
+++ UltraFlappy/ShulkerBox/Main.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace UltraFlappy\ShulkerBox;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use UltraFlappy\ShulkerBox\tile\ShulkerBox as ShulkerTile;
+use pocketmine\tile\Tile;
+use pocketmine\block\Block;
+use UltraFlappy\ShulkerBox\block\ShulkerBox;
+use pocketmine\block\BlockFactory;
+use pocketmine\item\Item;
+
+class Main extends PluginBase implements Listener {
+
+public static $shulkerEnable = true;
+
+public function onLoad(){
+	Tile::registerTile(ShulkerTile::class);
+	//BlockFactory::registerBlock(new ShulkerBox(Block::UNDYED_SHULKER_BOX), true);
+	BlockFactory::registerBlock(new ShulkerBox(), true);
+	Item::initCreativeItems();
+   }
+public function onEnable(){
+	$this->getServer()->getPluginManager()->registerEvents($this, $this);
+  }
+}
