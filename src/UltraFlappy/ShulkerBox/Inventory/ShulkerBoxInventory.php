359669d6c7702c5d18bb9cf0cf74c6bb8c74f564
Restyled.io <commits@restyled.io>
2021-05-27 06:47:30 +0000

    Restyled by php-cs-fixer

--- src/UltraFlappy/ShulkerBox/Inventory/ShulkerBoxInventory.php
+++ src/UltraFlappy/ShulkerBox/Inventory/ShulkerBoxInventory.php
@@ -12,67 +12,76 @@ use pocketmine\network\mcpe\protocol\LevelSoundEventPacket;
 use pocketmine\network\mcpe\protocol\types\WindowTypes;
 use pocketmine\Player;
 
-class ShulkerBoxInventory extends ContainerInventory {
+class ShulkerBoxInventory extends ContainerInventory
+{
 
 /**
  * @var ShulkerBox
  */
-protected $holder;
+    protected $holder;
 
-	public function __construct(ShulkerBox $tile){
-		parent::__construct($tile);
-	}
+    public function __construct(ShulkerBox $tile)
+    {
+        parent::__construct($tile);
+    }
 
-       /**
-        * @return string
-        */
-        public function getName(): string{
-		return ColorBlockMetaHelper::getColorFromMeta($this->getVariant()) . " Shulker Box";
-	}
-	public function getDefaultSize(): int{
-		return 27;
-	}
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return ColorBlockMetaHelper::getColorFromMeta($this->getVariant()) . " Shulker Box";
+    }
+    public function getDefaultSize(): int
+    {
+        return 27;
+    }
 
-	public function getNetworkType(): int{
-		return WindowTypes::CONTAINER;
-	}
+    public function getNetworkType(): int
+    {
+        return WindowTypes::CONTAINER;
+    }
 
-        /**
-	 * @param Player $who
-	 */
-	public function onOpen(Player $who): void{
-		parent::onOpen($who);
-		if(count($this->getViewers()) === 1 && ($level = $this->getHolder()->getLevel()) instanceof Level){
-			$this->broadcastBlockEventPacket($this->getHolder(), true);
-			$level->broadcastLevelSoundEvent($this->getHolder()->add(0.5, 0.5, 0.5), LevelSoundEventPacket::SOUND_SHULKERBOX_OPEN);
-		}
-	}
+    /**
+     * @param Player $who
+     */
+    public function onOpen(Player $who): void
+    {
+        parent::onOpen($who);
+        if (count($this->getViewers()) === 1 && ($level = $this->getHolder()->getLevel()) instanceof Level) {
+            $this->broadcastBlockEventPacket($this->getHolder(), true);
+            $level->broadcastLevelSoundEvent($this->getHolder()->add(0.5, 0.5, 0.5), LevelSoundEventPacket::SOUND_SHULKERBOX_OPEN);
+        }
+    }
 
- /**
-  * @return  ShulkerBox
-  */
-	public function getHolder(){
-		return $this->holder;
-	}
+    /**
+     * @return  ShulkerBox
+     */
+    public function getHolder()
+    {
+        return $this->holder;
+    }
 
-	protected function broadcastBlockEventPacket(Vector3 $vector, bool $isOpen){
-		$pk = new BlockEventPacket();
-		$pk->x = (int)$vector->x;
-		$pk->y = (int)$vector->y;
-		$pk->z = (int)$vector->z;
-		$pk->eventType = 1;
-		$pk->eventData = $isOpen ? 1 : 0;
-		$this->getHolder()->getLevel()->addChunkPacket($this->getHolder()->getX() >> 4, $this->getHolder()->getZ() >> 4, $pk);
-	}
+    protected function broadcastBlockEventPacket(Vector3 $vector, bool $isOpen)
+    {
+        $pk = new BlockEventPacket();
+        $pk->x = (int)$vector->x;
+        $pk->y = (int)$vector->y;
+        $pk->z = (int)$vector->z;
+        $pk->eventType = 1;
+        $pk->eventData = $isOpen ? 1 : 0;
+        $this->getHolder()->getLevel()->addChunkPacket($this->getHolder()->getX() >> 4, $this->getHolder()->getZ() >> 4, $pk);
+    }
  
-        /**
-	 * @param Player $who
-	 */
-	public function onClose(Player $who): void{
-		if(count($this->getViewers()) === 1 && ($level = $this->getHolder()->getLevel()) instanceof Level){
-			$this->broadcastBlockEventPacket($this->getHolder(), false);
-			$level->broadcastLevelSoundEvent($this->getHolder()->add(0.5, 0.5, 0.5), LevelSoundEventPacket::SOUND_SHULKERBOX_CLOSED);
-		}
-		parent::onClose($who);
-	}
+    /**
+     * @param Player $who
+     */
+    public function onClose(Player $who): void
+    {
+        if (count($this->getViewers()) === 1 && ($level = $this->getHolder()->getLevel()) instanceof Level) {
+            $this->broadcastBlockEventPacket($this->getHolder(), false);
+            $level->broadcastLevelSoundEvent($this->getHolder()->add(0.5, 0.5, 0.5), LevelSoundEventPacket::SOUND_SHULKERBOX_CLOSED);
+        }
+        parent::onClose($who);
+    }
 }
