89f5827de8832351b230913f0f6f74f1dfccd4a4
Restyled.io <commits@restyled.io>
2021-05-31 02:54:35 +0000

    Restyled by php-cs-fixer

--- src/UltraFlappy/ShulkerBox/Main.php
+++ src/UltraFlappy/ShulkerBox/Main.php
@@ -13,17 +13,19 @@ use UltraFlappy\ShulkerBox\Item\ShulkerBox as ShulkerItem;
 use pocketmine\block\BlockFactory;
 use pocketmine\item\Item;
 
-class Main extends PluginBase implements Listener {
-
-public function onLoad(){
-  Tile::registerTile(ShulkerTile::class);
-  BlockFactory::registerBlock(new ShulkerBox(Block::UNDYED_SHULKER_BOX), true);
-  BlockFactory::registerBlock(new ShulkerBox(), true);
-  ItemFactory::registerItem(new ShulkerItem(Block::SHULKER_BOX), true);
-  ItemFactory::registerItem(new ShulkerItem(Block::UNDYED_SHULKER_BOX), true);
-  Item::initCreativeItems();
-   }
-public function onEnable(){
-	$this->getServer()->getPluginManager()->registerEvents($this, $this);
-  }
+class Main extends PluginBase implements Listener
+{
+    public function onLoad()
+    {
+        Tile::registerTile(ShulkerTile::class);
+        BlockFactory::registerBlock(new ShulkerBox(Block::UNDYED_SHULKER_BOX), true);
+        BlockFactory::registerBlock(new ShulkerBox(), true);
+        ItemFactory::registerItem(new ShulkerItem(Block::SHULKER_BOX), true);
+        ItemFactory::registerItem(new ShulkerItem(Block::UNDYED_SHULKER_BOX), true);
+        Item::initCreativeItems();
+    }
+    public function onEnable()
+    {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
 }
