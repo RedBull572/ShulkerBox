2d9cc493140192bb1f36841fb4a079a2cf014b53
Flappy <60063027+UltraFlappy@users.noreply.github.com>
2021-06-13 18:39:35 +0700

    Removed some lines

--- src/UltraFlappy/ShulkerBox/Item/ShulkerBox.php
+++ src/UltraFlappy/ShulkerBox/Item/ShulkerBox.php
@@ -3,15 +3,15 @@
 namespace UltraFlappy\ShulkerBox\Item;
 
 use pocketmine\block\Block;
-
 use pocketmine\block\BlockFactory;
-
 use pocketmine\block\utils\ColorBlockMetaHelper;
-
 use pocketmine\item\Item;
 
 class ShulkerBox extends Item
 {
+
+ protected $id = "Shulker Box";
+
     public function __construct(int $id, int $meta = 0)
     {
         parent::__construct($id, $meta, $id = Block::UNDYED_SHULKER_BOX ? "Shulker Box" : ColorBlockMetaHelper::getColorFromMeta($meta)." Shulker Box");
