0fd8291f01a3093fdc2bda3a7f8c16bf6913fa9c
Flappy <60063027+UltraFlappy@users.noreply.github.com>
2021-06-20 14:03:37 +0700

    ShulkerBox is now craftable, soon for colorize

--- src/UltraFlappy/ShulkerBox/Main.php
+++ src/UltraFlappy/ShulkerBox/Main.php
@@ -12,6 +12,7 @@ use UltraFlappy\ShulkerBox\block\ShulkerBox;
 use UltraFlappy\ShulkerBox\Item\ShulkerBox as ShulkerItem;
 use pocketmine\block\BlockFactory;
 use pocketmine\item\Item;
+use pocketmine\inventory\ShapedRecipe;
 
 class Main extends PluginBase implements Listener
 {
@@ -23,7 +24,20 @@ class Main extends PluginBase implements Listener
         ItemFactory::registerItem(new ShulkerItem(Block::SHULKER_BOX), true);
         ItemFactory::registerItem(new ShulkerItem(Block::UNDYED_SHULKER_BOX), true);
         Item::initCreativeItems();
-    }
+        $this->getServer()->getCraftingManager()->registerShapedRecipe(new ShapedRecipe(
+				[
+					" A ",
+					" B ",
+					" A "
+				],
+				[
+					"A" => ItemFactory::get(Item::SHULKER_SHELL),
+					"B" => ItemFactory::get(Item::CHEST)
+				],
+				[ItemFactory::get(Item::UNDYED_SHULKER_BOX)]
+			)
+		);
+	}
     public function onEnable()
     {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
