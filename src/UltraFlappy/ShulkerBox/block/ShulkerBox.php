64da99bd7e176d1a0608ad87b6fcf396a45eaff1
Flappy <60063027+UltraFlappy@users.noreply.github.com>
2021-07-04 16:29:21 +0700

    Revert "Update ShulkerBox.php"

--- src/UltraFlappy/ShulkerBox/block/ShulkerBox.php
+++ src/UltraFlappy/ShulkerBox/block/ShulkerBox.php
@@ -54,8 +54,8 @@ class ShulkerBox extends Transparent
         }
         Tile::createTile(Tile::SHULKER_BOX, $this->getLevel(), $nbt);
 
-        if (!$player->isCreative()) {
-            $item->pop();
+        if ($player->isCreative()) {
+            ($inv = $player->getInventory())->clear($inv->getHeldItemIndex());
         }
         return true;
     }
